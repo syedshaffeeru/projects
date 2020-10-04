1911f86c4151505e5a319348e0f15ebd6ff9ec60
syedshaffeeru <syedshaffee@gmail.com>
2020-10-04 18:44:06 +0530

    Structure Change

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-alert('Hello World');
-var firstName = prompt("What is your first name");
-var lastName = prompt("What is your last name");
-var age = prompt("What is your age");
-console.log("Hi "+ firstName + " " + lastName + " have a great day!");
-console.log("Your age is " + age);
---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+// alert('Hello World');
+// var firstName = prompt("What is your first name");
+// var lastName = prompt("What is your last name");
+// var age = prompt("What is your age");
+// console.log("Hi "+ firstName + " " + lastName + " have a great day!");
+// console.log("Your age is " + age);
+
+var h1 = document.querySelector("h1");
+h1.style.color = "pink";
